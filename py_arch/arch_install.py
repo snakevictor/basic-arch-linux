8d64d727206005c3024bd4b4fac49ecf143fd940
Victor Ribeiro <113773800+snakevictor@users.noreply.github.com>
2024-11-13 23:25:31 -0300

    updated arch_install.py
    More comprehensive set of actions, also dropped python-pacman

--- py_arch/arch_install.py
+++ py_arch/arch_install.py
@@ -3,14 +3,13 @@ import shutil
 import subprocess
 import time
 
-import pacman  # type: ignore
 
 """
 Python script to setup Arch Linux with custom config files and packages
 """
 
 __author__ = "Victor Monteiro Ribeiro"
-__version__ = "0.3"
+__version__ = "0.4"
 __email__ = "victormribeiro.py@gmail.com"
 
 workdir = os.getcwd()
@@ -21,9 +20,16 @@ def check_exit():
     if check_exit == "y":
         exit(1)
 
+def run_bash(command):
+    process = subprocess.Popen(command, shell=True)
+    output, error = process.communicate()
+        if error:
+            print("\n", e)
+            check_exit()
+    return output
 
 # check if etc and home were downloaded:
-if not os.path.exists(workdir + "/etc") or not os.path.exists(workdir + "/home"):
+if not os.path.exists(workdir + "/assets/etc") or not os.path.exists(workdir + "/assets/home"):
     raise Exception(
         "etc or home were not correctly imported, check downloaded files and try again"
     )
@@ -34,14 +40,13 @@ if not os.path.exists("/etc") or not os.path.exists("/home"):
     raise Exception("system's etc or home were not created, check installation")
 print("system's etc and home verified...")
 
+command = '''echo "victor ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null'''
+run_bash(command)
+
 # running pacman -Syu to update system:
 print("\nUpdating system...\n")
 command = "sudo pacman -Syu --noconfirm"
-process = subprocess.Popen(command, shell=True)
-output, error = process.communicate()
-if error:
-    print("\n", error)
-    check_exit()
+run_bash(command)
 print("\nSystem updated!\n")
 
 # install packages from packages.txt:
@@ -51,25 +56,48 @@ with open(workdir + "/packages.txt", "r") as f:
 
 print(f"{len(packages_list)} found: {[x for x in packages_list]}.")
 
-for package in packages:
+for package in packages_list:
     try:
         print(f"\nInstalling {package}...\n")
-        pacman.install(package)
+        command = f"sudo pacman -S {package}"
+        run_bash(command)
     except Exception as e:
         print("\n", e)
         check_exit()
         continue
 
-print("\nInstallation complete!\n Importing config files...\n")
+print("\nInstallation complete!\n Building packages from git...\n")
 
-# copying assets from repo to system
-for dir in ["/etc", "/home"]:
-    assets = workdir + dir
-    shutil.copytree(assets, dir, dirs_exist_ok=True)
 
-print("Config files imported!\n")
+gitlist = [
+    "git clone --depth=1 https://github.com/adi1090x/rofi.git && cd rofi && chmod +x setup.sh && sudo ./setup.sh"
+    "sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
+    ]
+
+for command in gitlist:
+    try:
+        match word in command:
+            case word == "rofi":
+                package = "rofi"
+            case word == "":
+                package = ""
+            case word == "":
+                package = "
+            case word == "":
+                package = "
+
+        print(f"\nInstalling {package}...\n")
+        run_bash(command)
+    except Exception as e:
+        print("\n", e)
+        check_exit()
+        continue
 
 bashlist = [
+    "sudo mkdir -p /etc/systemd/system/getty@tty1.service.d",
+    '''echo -e "[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin victor --noclear %I \$TERM\nType=idle" | sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null''',
+    "sudo systemctl enable getty@tty1",
+    "sudo systemctl daemon-reload",
     "os-prober",
     "sudo grub-mkconfig -o /boot/grub/grub.cfg",
     "sudo mkinitcpio -p linux-zen",
@@ -77,18 +105,26 @@ bashlist = [
 
 for command in bashlist:
     match command:
+        case '''echo "$USER_NAME ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null''':
+            print("\nDisabling sudo password check...\n")
+        case "sudo mkdir -p /etc/systemd/system/getty@tty1.service.d":
+            print("\nActivating autologin...\n")
         case "sudo grub-mkconfig -o /boot/grub/grub.cfg":
             print("\nUpdating GRUB config...\n")
         case "sudo mkinitcpio -p linux-zen":
             print("\nUpdating initramfs...\n")
-    process = subprocess.Popen(command, shell=True)
-    output, error = process.communicate()
+    output = run_bash(command)
     if command == "os-prober":
         if not output:
-            print("No OS detected!")
-    if error:
-        print("\n", error)
-        check_exit()
+            print("No other OS detected!")
+
+# copying assets from repo to system
+for dir in ["/etc", "/home"]:
+    assets = workdir + "/assets" + dir
+    shutil.copytree(assets, dir, dirs_exist_ok=True)
+
+print("Config files imported!\n")
+
 
 print("\nAll done! Rebooting in...\n")
 
@@ -97,3 +133,4 @@ for i in range(5, 0, -1):
     time.sleep(1)
 
 os.system("shutdown -r now")
+
